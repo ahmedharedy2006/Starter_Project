1caa8a4d7dd69003a46ed04197f67f9ef18e74e1
Ahmed Haredy <ahassan200695@gmail.com>
2025-11-28 14:05:04 +0200

    init commit

---
+++ FougeraClub/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace FougeraClub.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+  
+       
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> AddEdit()
+        {
+            
+            return View();
+        }
+
+       
+
+        
+    }
+}
---
+++ FougeraClub/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Minimal services for a starter app
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+// PermissionScanner removed for starter project
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseCors("AllowAll");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
